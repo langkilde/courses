c92d9243a072a6455851e861a3ccfd55696e7c99
langkilde <daniel.langkilde@gmail.com>
2017-01-04 10:07:34 +0100

    add worksheet and example project

---
+++ coursera/functional_programming_specialization/functional_program_design/assignments/testing/src/functional_design.scala
@@ -0,0 +1,30 @@
+/**
+  * Created by langkilde on 1/4/17.
+  */
+object functional_design {
+
+  object week1 {
+
+    def streamRange(lo : Int, hi : Int) : Stream[Int] =
+      if (lo >= hi) Stream.empty
+      else Stream.cons(lo, streamRange(lo + 1, hi))
+
+    def listRange(lo : Int, hi : Int) : List[Int] =
+      if (lo >= hi) Nil
+      else lo :: listRange(lo + 1, hi)
+
+    def expr = {
+      val x = { print("x"); 1}
+      lazy val y = { print("y"); 2}
+      def z = { print("z"); 3}
+      z + y + x + z + y + x
+    }
+    print(expr)
+
+  }
+
+  def main(args: Array[String]) {
+    println(week1)
+  }
+
+}
