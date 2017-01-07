f4da83c48c03f45c385ff1beff6681e994b1072c
langkilde <daniel.langkilde@gmail.com>
2017-01-07 13:58:10 +0100

    add assignment quickcheck

--- coursera/functional_programming_specialization/functional_program_design/assignments/streams/src/main/scala/streams/Solver.scala
+++ coursera/functional_programming_specialization/functional_program_design/assignments/streams/src/main/scala/streams/Solver.scala
@@ -66,18 +66,29 @@ trait Solver extends GameDef {
    * construct the correctly sorted stream.
    */
   def from(initial: Stream[(Block, List[Move])],
-           explored: Set[Block]): Stream[(Block, List[Move])] = ???
+           explored: Set[Block]): Stream[(Block, List[Move])] = {
+    if (initial.isEmpty) Stream.empty
+    else {
+      val nextStep = for {
+        path <- initial
+        nextBlock <- newNeighborsOnly(neighborsWithHistory(path._1, path._2), explored)
+      } yield nextBlock
+      initial ++ from(nextStep, explored ++ (nextStep.map(_._1)))
+    }
+  }
 
   /**
    * The stream of all paths that begin at the starting block.
    */
-  lazy val pathsFromStart: Stream[(Block, List[Move])] = ???
+  lazy val pathsFromStart: Stream[(Block, List[Move])] = from(Stream((startBlock, List())), Set(startBlock))
 
   /**
    * Returns a stream of all possible pairs of the goal block along
    * with the history how it was reached.
    */
-  lazy val pathsToGoal: Stream[(Block, List[Move])] = ???
+  lazy val pathsToGoal: Stream[(Block, List[Move])] = pathsFromStart.filter(path => {
+    done(path._1)
+  })
 
   /**
    * The (or one of the) shortest sequence(s) of moves to reach the
@@ -87,5 +98,8 @@ trait Solver extends GameDef {
    * the first move that the player should perform from the starting
    * position.
    */
-  lazy val solution: List[Move] = ???
+  lazy val solution: List[Move] = {
+    if (pathsToGoal.isEmpty) List()
+    else pathsToGoal.minBy(path => path._2.length)._2.reverse
+  }
 }
