7fb0196abcc7b83f6bb19d0202d5bbd3876b63c8
langkilde <daniel.langkilde@gmail.com>
2017-01-17 00:41:14 +0100

    added first parts of calculator solution

---
+++ coursera/functional_programming_specialization/functional_program_design/assignments/calculator/src/main/scala/calculator/Polynomial.scala
@@ -0,0 +1,26 @@
+package calculator
+
+object Polynomial {
+  def computeDelta(a: Signal[Double], b: Signal[Double],
+      c: Signal[Double]): Signal[Double] = {
+    Signal {
+      b() * b() - 4 * a() * c()
+    }
+  }
+
+  def computeSolutions(a: Signal[Double], b: Signal[Double],
+      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
+    Signal {
+      val deltaRoot: Signal[Double] = Signal(math.sqrt(deltaRoot()))
+      val bNeg: Signal[Double]      = Signal(b() * -1)
+      val a2: Signal[Double]        = Signal(a())
+      if (deltaRoot() < 0) Set()
+      else {
+        Set(
+          (bNeg() + deltaRoot()) / a2(),
+          (bNeg() + deltaRoot()) / a2()
+        )
+      }
+    }
+  }
+}
